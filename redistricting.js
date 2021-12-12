21a9093a3d7c07986a58fc5c84cae9693a4999d3
amyprime <amy.finkbiner@gmail.com>
2021-12-12 11:16:22 -0500

    Move all HTML to JS

--- redistricting.js
+++ redistricting.js
@@ -11,12 +11,24 @@ var counties = [
 
 var baseUrl = "https://fdpa.github.io/maps/images";
 
-var dropdown = document.getElementById('countyDropdown');
+var dropdown;
 var chamberRadios;
 var sourceRadios;
-var mapImg = document.getElementById('mapImg');
+var mapImg;
 
 function initialize() {
+  document.getElementById('controlsAndMap').innerHTML =
+    "<div style=\"background: #00645e; float: left; width: 100%; margin-bottom: 20px; padding: 0 10px 10px 10px\">" +
+        "<span style=\"float:left; width: auto; padding: 0 10px 0 5px;\">" +
+          "<label style=\"color: #fff;\">County</label>" +
+          "<select id=\"countyDropdown\" onchange=\"displayMap()\"></select>" +
+        "</span>" +
+        "<div id=\"chamberRadioDiv\" style=\"float: left; padding: 0 5px 0 5px; min-width: 25%; margin: 0 10px 0 10px;\"></div>" +
+        "<div id=\"sourceRadioDiv\" style=\"float: left; padding: 0 5px 0 5px; min-width: 25%; margin: 0 10px 0 10px;\"></div>" +
+    "</div>" +
+    "<div><img id=\"mapImg\" /></div>";
+
+  dropdown = document.getElementById('countyDropdown');
   for (var i = 0; i < counties.length; i++) {
     var opt = document.createElement("option");
     opt.textContent = counties[i];
@@ -30,7 +42,7 @@ function initialize() {
     "<label style=\"font-weight: normal;\"><input type=\"radio\" name=\"chamberRadio\" value=\"pa_house\" onchange=\"displayMap()\" checked=\"checked\" />House</label>" +
     "<label style=\"font-weight: normal;\"><input type=\"radio\" name=\"chamberRadio\" value=\"pa_senate\" onchange=\"displayMap()\" />Senate</label>" +
 //    "<label><input type=\"radio\" name=\"chamberRadio\" value=\"us_congress\" onchange=\"displayMap()\" />Congress</label>";
-    "</span>";  
+    "</span>";
   chamberRadios = document.getElementsByName('chamberRadio');
 
   document.getElementById('sourceRadioDiv').innerHTML =
@@ -39,10 +51,10 @@ function initialize() {
     "<label style=\"font-weight: normal;\"><input type=\"radio\" name=\"sourceRadio\" value=\"current\" onchange=\"displayMap()\" checked=\"checked\" />Current</label>" +
 //    "<label style=\"font-weight: normal;\"><input type=\"radio\" name=\"sourceRadio\" value=\"lrc\" onchange=\"displayMap()\" />LRC</label>" +
     "<label style=\"font-weight: normal;\"><input type=\"radio\" name=\"sourceRadio\" value=\"fdpa\" onchange=\"displayMap()\" />FDPA</label>"
-    "</span>";  
-
+    "</span>";
   sourceRadios = document.getElementsByName('sourceRadio');
 
+  mapImg = document.getElementById('mapImg');
   mapImg.style.width = "500px";
   mapImg.style.height = "500px";
   
