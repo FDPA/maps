9d96eff734136d3944b21f7ae3699e1e03862a22
amyprime <amy.finkbiner@gmail.com>
2021-12-12 11:55:29 -0500

    Add interactive map links

--- redistricting.js
+++ redistricting.js
@@ -8,12 +8,82 @@ var counties = [
   "Somerset", "Sullivan", "Susquehanna", "Tioga", "Union", "Venango", "Warren", "Washington", "Wayne",
   "Westmoreland", "Wyoming", "York"
 ];
+var idByCounty = {
+  "Adams": "14RnRCTJtofBq-9WwwZ9w7BqZqmLcK4ZE",
+  "Allegheny": "1YyG76391-SHZ5wSNyfKtc0QAQPNP1UGP",
+  "Armstrong": "1X5EMiuBvKQYZcRnI11rP69XfYqO0fRjQ",
+  "Beaver": "18pci89eIopMBO0E2iujIVnp80C2sp6zg",
+  "Bedford": "1OAyDNHlNLVj_-pJzMHnekTyTH2eM5qNH",
+  "Berks": "1O8_f5cGBUNVGH0ofdBPipRe1IgIsexIj",
+  "Blair": "1cHZbJ4HJmwIoglAI4KSeGGm-w4UCvmwp",
+  "Bradford": "1zWtvAnoPzl3VVN4XwTNWluZKqY0N_557",
+  "Bucks": "1LBOoQzqw66P0d5q8C2425AJtOOaNcOCL",
+  "Butler": "1ITAbZ26ahNEmlhr4au23UCJITtvFRnki",
+  "Cambria": "13bGTLlkL73AKcGvqEjdZwVGSSpRCJFBO",
+  "Cameron": "13H97c9iiODQV8-URmSUtJSkYV5azityM",
+  "Carbon": "1byc0khibEL3lZuPit1ZPnLAxNrT_3zwy",
+  "Centre": "1ypOYqFW4TIloK9cGHl15qxir4nUq8VmE",
+  "Chester": "1yWEs4UUfO5a2Ux22ON6tY-nXg9ZA8H8A",
+  "Clarion": "1f7vl-gnmyIl_flCb98lna3VAEtrRD-Px",
+  "Clearfield": "1786MynbTkpKko4jEc7QomyO4c3APxn33",
+  "Clinton": "1qBvvf6zSinQTHeozIzbCE4-rk1d2gOpc",
+  "Columbia": "1RY8NMpbm8M8R5lszH7hhlkj7hoTVtVNl",
+  "Crawford": "12PvI6qROWwYPeoU8jDaPTmP5tpxQWffy",
+  "Cumberland": "1myvUkRtLlxQjtcB6jU6gwTOYyFXRwlTj",
+  "Dauphin": "10HPU1FREc_wq_9-oarMarcmiol99Evp_",
+  "Delaware": "1prIli3M1boLjh3fXGMxmQwEe0SFu1yBM",
+  "Elk": "1PEMu-jd6Lmzss-UM_eFwqkqd7TXJuCRI",
+  "Erie": "1ryVOyC5XwRFIHkMh8DhwVhSM2vchw4Dm",
+  "Fayette": "100T-k10KgJEyQYH-xhLHGgY6wIK0k8MS",
+  "Forest": "1rc5BJD4V0EnzVSw95mExFlTO_E2lxsn-",
+  "Franklin": "1JP-cMpqH59rRV9Y-QxxwfOZmiY7w20uY",
+  "Fulton": "19bvVgRGAl4T821W7Slu32CSc-ARuMRFy",
+  "Greene": "1XQVaQo3ydtS-pRlkQlozC4ZP00Oj6slc",
+  "Huntingdon": "1SUxMShvTxVWsaz4c5hMJ_2Wby7tTjh2-",
+  "Indiana": "1RRP1ltVb3RuHy2V5bi5EgJaU6G5P-_81",
+  "Jefferson": "1AGZqvJ3vIqxU3S5fJIkorE1l3GJk8kdZ",
+  "Juniata": "1VLcUOEauqmq3km9MP3-38OZaVUM4Gxho",
+  "Lackawanna": "10xc_Y7oBPIU4sZVD_9optVKjHmHsbVT9",
+  "Lancaster": "1xIF-mqL3HIxbnfrausXI1sy9B6JX7E95",
+  "Lawrence": "138uNDAeopbGWreK6wnP2ssEQTeOhhddL",
+  "Lebanon": "1azjkUGTSQ8ZrKR6J6yKzeK_nwdg53RGZ",
+  "Lehigh": "1c-_mkqmKOi9kgJQLYIxLsceqEPmW6hXl",
+  "Luzerne": "12u_dq-HpJWzFcqCeuNG8Hu4UOnkhjeDi",
+  "Lycoming": "1YdRZBkshz_nrZWDJfPPwFbNo--nDSIUD",
+  "McKean": "1xKzKGp7Z9LyI8CQkKeEegaKWR5iI6zNH",
+  "Mercer": "1r14UrYgsEgRn_DPDngRvZs3V7HgizY_8",
+  "Mifflin": "1XFDENfO7uKw4x3Jhpku6z-PQqs2ZWH-a",
+  "Monroe": "1GURToaJ1319YNVDJWI_OXDvRee9KoZiA",
+  "Montgomery": "1wpUF89RTGFyaQ-0TSYazaz9QkUa6Ry1j",
+  "Montour": "1w-Kou_F1jhQELhrcmy6BLXZ9mZQZchqX",
+  "Northampton": "1SuwNpLWUaJzSg5aMRrAQ1lvxIWLHKORq",
+  "Northumberland": "1VjNJKb7N_9SumCL0ujY4xuLBorM42ATo",
+  "Perry": "1qahFVQmrK3rKorhepOiNXnCSQ8ti6rBa",
+  "Philadelphia": "1Pf2DcKHeyvsUwjQSb3MiSe3_3dYV6dDC",
+  "Pike": "1VzhvZUai8qAFSlKAr0WYK4DhbThuUHMt",
+  "Potter": "1466940eApsvRHA7YxiKfXe5xskPgBinV",
+  "Schuylkill": "17sx4HJyGW736uy3BXGtt5FHEI490Xwi_",
+  "Snyder": "10fhuO8R1ybaXVNMiyxk9AQszquKGX05q",
+  "Somerset": "1baJYcVGac-nLimW09AIOtmkBnBSrQbKj",
+  "Sullivan": "1tmtl4WCKcuojFuhnJ6VxBB2OuwBzmVnJ",
+  "Susquehanna": "1_Ddq7ujQ_66woiqa-WJ9ibcU5-vXE9sK",
+  "Tioga": "1DkrpQ4jnb21xoIUN0UmedYbZr44BlX44",
+  "Union": "1mTEnT5kDPk1LCJOZhv1TOLafj12atqpk",
+  "Venango": "1eVlBewyEsERA5bZetrr-5WX7tKK78NAM",
+  "Warren": "1jozfLvHuMB2YdIXF40f0FgcpobieYDo-",
+  "Washington": "13Q6Vi5aEXIqPdv7ba-UB_4pHjuz4AemT",
+  "Wayne": "1ytNSYtKf2ZwQnABfWBs_LYQuACgdnbj-",
+  "Westmoreland": "1hONhE2nLZRn6Mt8dgovqOoTrOHQ5bnhM",
+  "Wyoming": "1UTMHow00ByEDD2ThaCevmz5wxNB4a9wV",
+  "York": "1BNPxE_1D6jSdcgbNQL7sAVSkXtdgseSC"
+};
 
 var baseUrl = "https://fdpa.github.io/maps/images";
 
 var dropdown;
 var chamberRadios;
 var sourceRadios;
+var interactiveMapLink;
 var mapImg;
 
 function initialize() {
@@ -26,6 +96,7 @@ function initialize() {
         "<div id=\"chamberRadioDiv\" style=\"float: left; padding: 0 5px 0 5px; min-width: 25%; margin: 0 10px 0 10px;\"></div>" +
         "<div id=\"sourceRadioDiv\" style=\"float: left; padding: 0 5px 0 5px; min-width: 25%; margin: 0 10px 0 10px;\"></div>" +
     "</div>" +
+    "<div id=\"interactiveMapLink\"></div>" +
     "<div><img id=\"mapImg\" /></div>";
 
   dropdown = document.getElementById('countyDropdown');
@@ -54,6 +125,8 @@ function initialize() {
     "</span>";
   sourceRadios = document.getElementsByName('sourceRadio');
 
+  interactiveMapLink = document.getElementById('interactiveMapLink');
+
   mapImg = document.getElementById('mapImg');
   mapImg.style.width = "500px";
   mapImg.style.height = "500px";
@@ -63,6 +136,9 @@ function initialize() {
 
 function displayMap() {
   var county = dropdown.value;
+  var prettyCounty = county[0].toUpperCase() + county.substring(1);
+  interactiveMapLink.innerHTML = "You can also view an <a href=\"https://www.google.com/maps/d/edit?mid=" + idByCounty[prettyCounty] + "\" target=\"_blank\">interactive map</a> for " + prettyCounty + " County.";
+
   var chamber = getRadioValue(chamberRadios);
   var source = getRadioValue(sourceRadios);
   var sourceString = "";
